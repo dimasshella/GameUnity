03221d450cacc4f753f71bd1dabbcf693957eb31
Hesti <hestianisa94@gmail.com>
2020-04-01 20:36:14 +0700

    page4
    dan lainlain

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Text pattyText;
+    private int totalPatty = 0;
+    void Start()
+    {
+        UpdatePattyText();
+    }
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Patty"))
+        {
+            totalPatty++;
+            UpdatePattyText();
+            Destroy(hit.gameObject);
+        }
+    }
+    private void UpdatePattyText()
+    {
+        string pattyMessage = "patty = " + totalPatty;
+        pattyText.text = pattyMessage;
+    }
+
+}
